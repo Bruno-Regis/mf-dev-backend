8724c08687f2342a2dd914827dcaa1a95a4a0e51
Bruno-Regis <149000097+Bruno-Regis@users.noreply.github.com>
2025-10-05 18:49:50 -0300

    Configuração do BD e da TB Veiculo

--- mf-dev-backend/Program.cs
+++ mf-dev-backend/Program.cs
@@ -1,9 +1,16 @@
+using mf_dev_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+//Quando o programa iniciar, ele vai chamar essa configuração de BD passando a string de conexão
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
