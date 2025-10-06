7ff272e84e92713b18f035f86af921560318e44c
Bruno-Regis <149000097+Bruno-Regis@users.noreply.github.com>
2025-10-06 19:22:08 -0300

    CRUD Veiculos

--- mf-dev-backend/Models/Veiculo.cs
+++ mf-dev-backend/Models/Veiculo.cs
@@ -13,9 +13,10 @@ namespace mf_dev_backend.Models
         [Required(ErrorMessage = "Obrigatório informar a placa!")]
         public string Placa { get; set; }
         [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        [Display(Name ="Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
         [Required(ErrorMessage = "Obrigatório informar o ano do modelo!")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
-
     }
 }
