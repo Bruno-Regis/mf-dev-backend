d2c0b6d02bb9347142f9a635f5478b221d61ec5c
Bruno-Regis <149000097+Bruno-Regis@users.noreply.github.com>
2025-10-09 17:10:01 -0300

    Correção de bug no Model de Usuario

--- mf-dev-backend/Models/Usuario.cs
+++ mf-dev-backend/Models/Usuario.cs
@@ -17,7 +17,7 @@ namespace mf_dev_backend.Models
         public string Senha { get; set; }
 
         [Required(ErrorMessage = "Obeigatório informar o perfil de usuário")]
-        public string Perfil { get; set; }
+        public Perfil Perfil { get; set; }
 
     }
 
